42eced77870d4b26fca854145ae34a026ead8fc4
MineBartekSA-Dell <skoczen88@op.pl>
2016-11-04 17:49:45 +0100

    Adding a TerraSpleef project

---
+++ TerraSpleef/TerraSpleef.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using TShockAPI;
+using TShockAPI.DB;
+using TerrariaApi.Server;
+using Terraria;
+
+namespace TerraSpleef
+{
+    public class TerraSpleef : TerrariaPlugin
+    {
+        //Vars
+
+        //Load stage
+        public override string Name
+        {
+            get { return "TerraSpleef"; }
+        }
+        public override Version Version
+        {
+            get { return new Version(1, 0, 0); }
+        }
+        public override string Author
+        {
+            get { return "MineBartekSA"; }
+        }
+        public override string Description
+        {
+            get { return "Spleef minigame plugin!"; }
+        }
+        public TerraSpleef(Main game) : base(game)
+        {
+            //Nothing!
+        }
+        public override void Initialize()
+        {
+
+            //Hooks
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //UnHooks
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+        //End of loading stage
+
+
+
+        //Voids
+
+
+
+        //Commands
+        void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command(spleefCMD, "terraspleef", "tspleef", "ts")
+            {
+                HelpText = "A TerraSpleef base command"
+            });
+        }
+        //End of Commands
+
+
+
+        //Commands exe voids
+        void spleefCMD(CommandArgs args)
+        {
+
+        }
+        //End of Commands exe voids
+
+
+
+        //Other voids
+        //End of Other voids
+    }
+}
